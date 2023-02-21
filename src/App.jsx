883fd5bc2590748357ae1a62a65daac7c5c5372e
Subham Chowdhury <itz_subhamrock@outlook.com>
2023-02-21 14:13:30 +0530

    First Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import Navbar from './Components/Navbar/Navbar';
+import Search from "./Components/SearchDiv/Search.jsx";
+import Jobs from "./Components/JobDiv/Jobs.jsx";
+import Value from "./Components/ValueDiv/Value.jsx";
+import Footer from "./Components/FooterDiv/Footer.jsx";
 
 function App() {
     return (
         <div>
-            <h1>Hello World!</h1>
+            <Navbar/>
+            <Search/>
+            <Jobs/>
+            <Value/>
+            <Footer/>
         </div>
     );
 }
