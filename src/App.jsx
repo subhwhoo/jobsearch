f9b8ed620401a306b2c062d1363e74bf76c1b230
Subham Chowdhury <itz_subhamrock@outlook.com>
2023-02-22 20:49:05 +0530

    Solved the Issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import Navbar from './Components/Navbar/Navbar';
+import React from "react";
+import Navbar from "./Components/NavBar/NavBar.jsx";
 import Search from "./Components/SearchDiv/Search.jsx";
 import Jobs from "./Components/JobDiv/Jobs.jsx";
 import Value from "./Components/ValueDiv/Value.jsx";
 import Footer from "./Components/FooterDiv/Footer.jsx";
 
 function App() {
-    return (
-        <div>
-            <Navbar/>
-            <Search/>
-            <Jobs/>
-            <Value/>
-            <Footer/>
-        </div>
-    );
+  return (
+    <div className={"w-[85%] m-auto bg-white"}>
+      <Navbar />
+      <Search />
+      <Jobs />
+      <Value />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
